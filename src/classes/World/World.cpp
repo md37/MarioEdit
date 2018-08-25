6a7e95cca4b81d1d4333edc244579579edaf5258
Marcin Kukliński <senghe@gmail.com>
2018-08-25 09:10:29 +0200

    remove hills

--- src/classes/World/World.cpp
+++
@@ -1,8 +0,0 @@
-#include "World.hpp"
-
-
-World::World(std::shared_ptr<sf::RenderWindow> window, std::shared_ptr<Grid> grid, std::shared_ptr<TileSet> tileSet) {
-    this->window = window;
-    this->grid = grid;
-    this->tileSet = tileSet;
-}

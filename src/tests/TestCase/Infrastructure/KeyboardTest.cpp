bff8e2cba3adaa8e254294e0a8ed376ecbe1451a
Marcin Kukliński <senghe@gmail.com>
2018-12-09 12:39:08 +0100

    Keyboard numeric tests

--- src/tests/TestCase/Infrastructure/KeyboardTest.cpp
+++ src/tests/TestCase/Infrastructure/KeyboardTest.cpp
@@ -34,3 +34,24 @@ TEST(KeyboardTest, test_released_two_times) {
     keyboard.release(sf::Keyboard::Key::Space);
     ASSERT_FALSE(keyboard.isPressed(sf::Keyboard::Key::Space));
 }
+
+TEST(KeyboardTest, test_no_numeric_key_pressed) {
+    Keyboard keyboard;
+    ASSERT_FALSE(keyboard.isNumericKeyPressed());
+    ASSERT_FALSE(keyboard.getPressedNumeric().has_value());
+}
+
+TEST(KeyboardTest, test_numeric_key_0_is_pressed) {
+    sf::Keyboard::Key keys[10] = {
+        sf::Keyboard::Key::Num0, sf::Keyboard::Key::Num1, sf::Keyboard::Key::Num2, sf::Keyboard::Key::Num3,
+        sf::Keyboard::Key::Num4, sf::Keyboard::Key::Num5, sf::Keyboard::Key::Num6, sf::Keyboard::Key::Num7,
+        sf::Keyboard::Key::Num8, sf::Keyboard::Key::Num9
+    };
+
+    for (auto key: keys) {
+        Keyboard keyboard;
+        keyboard.press(key);
+        ASSERT_TRUE(keyboard.isNumericKeyPressed());
+        ASSERT_TRUE(keyboard.getPressedNumeric().has_value());
+    }
+}
